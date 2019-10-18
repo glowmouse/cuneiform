d367a8765cb1569253d92d382bbe38e3b2f38247
Andrew Brownbill <andrew.brownbill@gmail.com>
2019-10-18 19:28:10 -0400

    Adapt focuser code to logger

---
+++ firmware/net_interface.h
@@ -0,0 +1,55 @@
+#ifndef __NetInterface_H__
+#define __NetInterface_H__
+
+#include <string>
+#include "hardware_interface.h"
+#include "debug_interface.h"
+
+class WifiOstream;
+class WifiDebugOstream;
+
+/// @brief Interface to the client
+///
+/// This class's one job is to provide an interface to the client.
+///
+class NetInterface {
+  public:
+
+  struct category : public beefocus_tag {};
+  using char_type = char;
+
+  NetInterface()
+  {
+  }
+  virtual ~NetInterface()
+  {
+  }
+
+  virtual void setup( DebugInterface &debugLog ) = 0;
+
+  virtual bool getString( WifiDebugOstream &log, std::string& string ) = 0;
+  virtual std::streamsize write( const char_type* s, std::streamsize n ) = 0;
+  virtual void flush() = 0;
+
+  private:
+};
+
+class NetConnection {
+  public:
+
+  struct category : public beefocus_tag {};
+  using char_type = char;
+
+  virtual bool getString( WifiDebugOstream& lot, std::string& string )=0;
+  virtual operator bool( void ) = 0;
+  virtual void reset( void ) = 0;
+  virtual std::streamsize write( const char_type* s, std::streamsize n ) = 0;
+  virtual void flush() = 0;
+
+  protected:
+
+
+};
+
+#endif
+
---
+++ firmware_sim/main.cpp
@@ -0,0 +1,115 @@
+
+#include <iostream>
+#include <memory>
+#include <unistd.h>
+
+#include "focuser_state.h"
+#include "hardware_interface.h"
+
+std::unique_ptr<FS::Focuser> focuser;
+
+class NetInterfaceSim: public NetInterface {
+  public:
+
+  struct category: virtual beefocus_tag {};
+  using char_type = char;
+
+  void setup( DebugInterface& debugLog ) override
+  {
+    debugLog << "Simulator Net Interface Init\n";
+  }
+  bool getString( WifiDebugOstream& log, std::string& input ) override
+  {
+    fd_set readfds;
+    FD_ZERO(&readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+
+    FD_SET(STDIN_FILENO, &readfds );
+    if ( select(1, &readfds, nullptr, nullptr, &timeout ))
+    {
+      std::cin >> input;
+      return true;
+    }
+    input = "";
+    return false;
+  }
+  std::streamsize write( const char_type* s, std::streamsize n ) override
+  {
+    for ( std::streamsize i = 0; i < n; ++i ) {
+      std::cout << s[i];
+    }
+    return n;
+  }
+  void flush() override
+  {
+  }
+};
+
+class HWISim: public HWI
+{
+  public: 
+
+  void PinMode( Pin pin, PinIOMode mode ) override
+  {
+    std::cout << "PM (" << HWI::pinNames.at(pin) << ") = " << HWI::pinIOModeNames.at(mode) << "\n";
+  }
+  void DigitalWrite( Pin pin, PinState state ) override
+  {
+    const std::string name = HWI::pinNames.at(pin);
+    std::cout << "DW (" << HWI::pinNames.at(pin) 
+              << ") = " << HWI::pinStateNames.at( state ) 
+              << "\n";
+  }
+  PinState DigitalRead( Pin pin ) override
+  {
+    std::cout << "DR " << HWI::pinNames.at(pin) << " returning HOME_INACTIVE";
+    return HWI::PinState::DUMMY_INACTIVE;
+  }
+};
+
+class DebugInterfaceSim: public DebugInterface
+{
+  struct category: virtual beefocus_tag {};
+  using char_type = char;
+
+  std::streamsize write( const char_type* s, std::streamsize n ) override
+  {
+    // Ignore for now.
+    return n;
+  }
+  void disable() override 
+  {
+    // Can't disable what we're ignoring.
+  }
+};
+
+void loop() {
+  focuser->loop();
+}
+
+void setup() {
+  std::unique_ptr<NetInterface> wifi( new NetInterfaceSim );
+  std::unique_ptr<HWI> hardware( new HWISim );
+  std::unique_ptr<DebugInterface> debug( new DebugInterfaceSim );
+  FS::BuildParams params( FS::Build::LOW_POWER_HYPERSTAR_FOCUSER );
+  focuser = std::unique_ptr<FS::Focuser>(
+     new FS::Focuser( 
+        std::move(wifi), 
+        std::move(hardware),
+				std::move(debug),
+        params )
+  );
+}
+
+int main(int argc, char* argv[])
+{
+  setup();
+  for ( ;; ) 
+  {
+    loop();
+  }
+}
+
